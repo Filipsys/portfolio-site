3fa6c502d6b5148388891cfeb2b0ff9fd6918c84
filyys <filipsysak@gmail.com>
2024-09-11 19:16:01 +0200

    Astro migration 3/3. Fixed hover card, fixed fonts (again), fixed background mask effect and grid.

---
+++ src/components/HoverCardComponent.tsx
@@ -0,0 +1,21 @@
+import {
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+} from "@/components/ui/hover-card";
+import { InfoIcon } from "@/icons/dev-icons";
+
+export const HoverCardComponent = () => {
+  return (
+    <HoverCard openDelay={50}>
+      <HoverCardTrigger className="z-50 *:hidden *:size-4 *:sm:block">
+        <InfoIcon />
+      </HoverCardTrigger>
+      <HoverCardContent className="p-2 bg-[#09090b] border-white/10">
+        <p className="text-xs text-white">
+          The stars indicate my level of proficiency in each technology.
+        </p>
+      </HoverCardContent>
+    </HoverCard>
+  );
+};
