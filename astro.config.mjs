3d5ffcfde36c8dcbce6563b6347db00717b88041
filyys <filipsysak@gmail.com>
2024-09-17 21:52:13 +0200

    Added sitemap config to Astro, changed email to a spam-protected mail and fixed background grid showing up on page load

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  site: "https://filipsysak.vercel.app",
+  integrations: [react(), tailwind({ applyBaseStyles: false }), sitemap()],
 });
