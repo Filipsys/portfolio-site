1439bd55ac108ce73a8986ee80a8767c7d0a3f4e
Filipsys <filipsysak@gmail.com>
2024-11-09 18:52:45 +0100

    Removed i18n from astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import astroI18next from "astro-i18next";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -11,7 +9,6 @@ export default defineConfig({
   site: "https://filipsysak.vercel.app",
   integrations: [
     react(),
-    astroI18next(),
     tailwind({ applyBaseStyles: false }),
     sitemap(),
   ],
