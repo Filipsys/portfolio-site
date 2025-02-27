86b3f45ec69907f05cc3d93361c9d760e86a985d
Filipsys <filipsysak@gmail.com>
2025-02-27 23:56:31 +0100

    Christmas lights fix, theme tweak, grid hover fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,61 @@
-// import { ModeToggle } from "@/app/components/ModeToggle";
+"use client";
+
 import { HomeComponent } from "@/components/Home";
-import Link from "next/link";
-import Layout from "@/app/layout";
 import { Separator } from "@/components/Separator";
 import { Grid } from "@/components/Grid";
+import { ChristmasLights } from "@/components/ChristmasLights";
+import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [isChristmas, setIsChristmas] = useState(false);
+  const movableGridRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const currentMonth = new Date().getMonth() + 1;
+
+    if ([12, 1].includes(currentMonth)) setIsChristmas(true);
+  }, []);
+
   return (
-    <Layout>
-      <main className="bg-background text-foreground font-overpass w-full" id="hover-div">
-        {/* <div className="absolute left-0 top-0 z-10 h-screen w-full hidden lg:block overflow-hidden">
-          <div
-            className="lights absolute -inset-2 -translate-x-20 dark:hidden"
-            style={{
-              display: "none",
-            }}
-          >
-            light1
-          </div>
-          <div
-            className="lights absolute -inset-2 left-0 translate-x-1/2 w-full dark:hidden"
-            style={{
-              display: "none",
-            }}
-          >
-            light2
-          </div>
-          <div
-            className="lights absolute -translate-y-2 translate-x-2 right-0 dark:hidden"
-            style={{
-              display: "none",
-            }}
-          >
-            light3
-          </div>
-        </div> */}
+    <main
+      className="bg-background text-foreground font-overpass w-full"
+      id="hover-div"
+      onMouseMove={(event) => setCursorPosition({ x: event.clientX, y: event.clientY })}
+      onMouseOut={() => (movableGridRef.current ? (movableGridRef.current.style.opacity = "0") : null)}
+      onMouseOver={() => (movableGridRef.current ? (movableGridRef.current.style.opacity = "1") : null)}
+    >
+      {isChristmas ? <ChristmasLights /> : null}
 
-        {/* <div className="absolute top-0 right-0 z-50 p-2"><ModeToggle /></div> */}
+      <div className="h-max items-center justify-center md:flex-col lg:mx-16 lg:flex lg:flex-row xl:mx-40 2xl:mx-80">
+        <nav className="block h-fit w-full p-8 lg:w-1/5 2xl:w-2/5">
+          <ul className="flex select-none justify-center gap-x-8 text-xl tracking-tight sm:gap-x-16 sm:text-2xl lg:flex-col lg:items-end lg:text-3xl">
+            <li className="z-50 cursor-pointer">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="line-through">
+              <a href="/about">About</a>
+            </li>
+            <li className="line-through">
+              <a href="/notes">Notes</a>
+            </li>
+            <li className="line-through">
+              <a href="/projects">Projects</a>
+            </li>
+          </ul>
+        </nav>
 
-        <div className="h-max items-center justify-center md:flex-col lg:mx-16 lg:flex lg:flex-row xl:mx-40 2xl:mx-80">
-          <nav className="block h-fit w-full p-8 lg:w-1/5 2xl:w-2/5">
-            <ul className="flex select-none justify-center gap-x-8 text-xl tracking-tight sm:gap-x-16 sm:text-2xl lg:flex-col lg:items-end lg:text-3xl">
-              <li className="z-50 cursor-pointer">
-                <Link href="/">Home</Link>
-              </li>
-              <li className="line-through">
-                <a href="/about">About</a>
-              </li>
-              <li className="line-through">
-                <a href="/notes">Notes</a>
-              </li>
-              <li className="line-through">
-                <a href="/projects">Projects</a>
-              </li>
-            </ul>
-          </nav>
-
-          <div className="hidden h-dvh lg:flex lg:items-center *:h-1/3">
-            <Separator vertical />
-          </div>
-
-          <main className="flex z-50 h-max w-full items-center justify-center p-6 text-xl lg:h-dvh lg:w-4/5">
-            <HomeComponent />
-          </main>
+        <div className="hidden h-dvh lg:flex lg:items-center *:h-1/3">
+          <Separator vertical />
         </div>
 
-        <Grid />
-      </main>
-    </Layout>
+        <main className="flex z-50 h-max w-full items-center justify-center p-6 text-xl lg:h-dvh lg:w-4/5">
+          <HomeComponent />
+        </main>
+      </div>
+
+      <Grid cursorPosition={cursorPosition} ref={movableGridRef} />
+    </main>
   );
 }
