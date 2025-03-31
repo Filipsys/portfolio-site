45afcc4f9ed1b6666cee0c1ca0a4bdcbaf3d1404
Filipsys <filipsysak@gmail.com>
2025-03-31 23:22:11 +0200

    Eslint disabled no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,11 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+  },
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
 
 export default eslintConfig;
