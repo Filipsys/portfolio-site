8657634889938965ebb260d4e484dcc1d0397533
Filipsys <filipsysak@gmail.com>
2025-04-03 00:36:13 +0200

    Added more accurate metadata to the site

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### TODO:
 
-- [ ] Fix metadata on the main site.
+- [x] Fix metadata on the main site.
 - [x] Fix fun mode view and add a theme toggle to switch between.
 - [ ] Add snow during the winter period in the main site.
 - [ ] Migrate project to Tailwind v4 & React 19.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://filyys.dev"),
   title: "filyys - main page",
   description: "My simple, small, personal portfolio site",
+  openGraph: {
+    type: "website",
+    title: "filyys - main page",
+    siteName: "filyys - main page",
+    description: "My simple, small, personal portfolio site",
+    url: "https://filyys.dev",
+    images: [{ url: "https://filyys.dev/fire.gif" }],
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -12,7 +21,13 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en">
       <head>
         <meta name="description" content="My simple, small, personal portfolio site" />
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="keywords"
+          content="portfolio, personal, developer, software, Next.js, React, projects"
+        />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <title>filyys - main page</title>
 
