dbd7d1ae78372a8a5bb50973afb25d6d691869bb
Filipsys <filipsysak@gmail.com>
2025-08-21 23:59:35 +0200

    Initial page for terms

---
+++ src/app/[locale]/fun/terms/layout.tsx
@@ -0,0 +1,15 @@
+import "./fun.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <title>terms @ filyys</title>
+      </head>
+
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/fun/terms/page.tsx
@@ -0,0 +1,3 @@
+export default function Fun() (
+    <div>Terms</div>
+  );
