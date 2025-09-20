87c789ae91f889a529e9a3ef4b9dc28fc908744b
Filipsys <filipsysak@gmail.com>
2025-09-20 23:57:37 +0200

    Completed grid for CAPTCHA

--- src/components/fun/CAPTCHA.tsx
+++ src/components/fun/CAPTCHA.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
+const SelectBox = () => {
+  const [selected, setSelected] = useState<boolean>(false);
+
+  return (
+    <div
+      onClick={() => setSelected(!selected)}
+      className={selected ? "transition-colors duration-75 bg-white/20" : ""}
+    />
+  );
+}
+
 export const CAPTCHA = () => {
+  const gridSize = { width: 3, height: 3 };
+
   return (
     <div className="absolute z-100 bg-white text-black ">
-      <div className="w-full p-2">
+      <div className="flex flex-col gap-1 w-full p-2">
         <div className="flex justify-between bg-[#1973e8] text-white p-4">
           <div>
             <p>Select all squares with</p>
@@ -9,20 +24,12 @@ export const CAPTCHA = () => {
             <p>If there are none, click skip</p>
           </div>
 
-          <div className="size-24 bg-white" />
+          <img src="snippets/snippet1.png" className="size-24 aspect-square border-1 border-white" />
         </div>
 
         <div className="grid bg-black">
-          <div className="w-[400px] [grid-area:1/1] z-10 aspect-square grid grid-cols-3 grid-rows-3 bg-white gap-1 *:bg-black">
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
+          <div className={`w-[400px] [grid-area:1/1] z-10 aspect-square grid grid-cols-${gridSize.width} grid-rows-${gridSize.height} *:border-2 *:border-white`}>
+            {Array.from({ length: gridSize.width * gridSize.height }).map(() => <SelectBox />)}
           </div>
 
           <img src="snippets/snippet1.png" className="[grid-area:1/1] z-0 top-0 w-[400px] aspect-square" />
