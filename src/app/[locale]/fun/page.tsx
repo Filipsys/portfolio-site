0af8a587da157bd2af671ba40573fe5c0320f3ac
Filipsys <filipsysak@gmail.com>
2025-10-14 23:53:27 +0200

    Changed background

--- src/app/[locale]/fun/page.tsx
+++ src/app/[locale]/fun/page.tsx
@@ -22,12 +22,22 @@ export default function Fun() {
           className="text-white h-dvh w-full flex justify-center"
           id="main"
           style={{
-            backgroundImage: "url(/background3.webp)",
-            backgroundSize: "10vw 10vw",
+            backgroundImage: "url(/glitch.webp)",
+            backgroundSize: "30vw 30vw",
           }}
         >
           <div className="bg-zinc-950 max-w-7xl [font-family:serif] flex flex-col h-dvh [box-shadow:_0px_0px_50px_30px_rgba(0,_0,_0,_1)]">
-            <p>HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello HelloHelloHelloHello</p>
+            <div className="w-full h-dvh flex flex-row">
+              <div className="h-full w-2/3">
+                <div className="w-full flex justify-center">
+                  <MainPanel />
+                </div>
+              </div>
+
+              <div className="h-full w-1/3">
+                <SidePanel />
+              </div>
+            </div>
           </div>
         </main>
       </>
